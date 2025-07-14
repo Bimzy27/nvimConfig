87cfae3d5f81903f85eb22c2a73f57df3ec77bb8
Bimzy27 <branden.immerzeel@gmail.com>
2025-07-14 11:15:38 +0930

    updating plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,14 @@
-print("Hello from Treesitter")
-
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    -- Set install options inside the config block
-    require("nvim-treesitter.install").prefer_git = false
-    require("nvim-treesitter.install").compilers = { "clang", "gcc" }
-
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "cpp", "lua" },
-      sync_install = false,
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-      },
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  }
 }
--- lua/plugins/ui.lua
+++
@@ -1,9 +0,0 @@
-print("Hello from ui")
-
-vim.opt.number = true
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
-return {}
